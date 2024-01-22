a0f315a447445ac8bdeebf79d6c12e4fc2de3db1
Jessica Jimenez <jimenez.jessica132@gmail.com>
2024-01-22 12:56:25 -0500

    created new styled file for post-auth pages

---
+++ src/styles/postAuthStyles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const LargeTitleStyle = styled.h2`
+  font-size: 20px;
+  color: var(--largeTextColor);
+  //@media (max-width: 640px) {
+  //  font-size: 15px;
+  //}
+`;
+
+export const TitleStyle = styled.h3`
+  font-size: 17px;
+  //letter-spacing: -2px;
+  color: var(--largeTextColor);
+  //@media (max-width: 960px) {
+  //  font-size: 24px;
+  //}
+`;
+
+export const SubtitleStyle = styled.h3`
+  font-size: 14px;
+  //line-height: 150%;
+  color: var(--primary);
+  //@media (max-width: 960px) {
+   // font-size: 20px;
+  //}
+`;
+
+export const NormalTextStyle = styled.p`
+  font-size: 14px;
+  //letter-spacing: normal;
+  //line-height: 150%;
+  color: var(--smallTextColor);
+  //@media (max-width: 960px) {
+  //  font-size: 16px;
+  //}
+`;
